aef95baa49c5158ae5f0f8bddc4e6c30aa923368
abhinav <abhinavisgreat18@gmail.com>
2025-06-23 20:00:59 +0530

    feat : navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className='p-2'>
+      <Link href={'/'} className='flex gap-1 items-center'>
+        <p className='bg-secondary w-fit py-1 px-2.5 rounded-full font-extrabold'>
+          A
+        </p>
+        <p className='text-secondary font-extrabold text-xl'>Artistly</p>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;

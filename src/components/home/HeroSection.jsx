07a24a4e4c380db8440f648aee4c01579482feef
abhinav <abhinavisgreat18@gmail.com>
2025-06-24 00:37:56 +0530

    updated herosection ui

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { NotebookPen, Sparkle } from "lucide-react";
 
 const HeroSection = () => {
   return (
@@ -14,7 +15,7 @@ const HeroSection = () => {
         </p>
         <div className='mt-10 flex gap-2 justify-center'>
           <Button className='bg-black rounded-full text-lg p-5'>
-            <p className="cursor-pointer">Explore</p>
+            <p className='cursor-pointer'>Explore</p>
           </Button>
           <Button className='bg-stone-200 text-black  text-lg p-5 hover:bg-stone-300 rounded-full'>
             Join as artist
@@ -23,15 +24,51 @@ const HeroSection = () => {
       </div>
       <div className='absolute bottom-0 w-[98%] flex items-end gap-2'>
         <div className='w-full h-96 flex flex-col gap-2'>
-          <div className='w-full h-3/4 bg-red-50 rounded-3xl'></div>
-          <div className='w-full h-1/4  bg-red-50 rounded-3xl'></div>
+          <div className='w-full h-3/4 bg-secondary rounded-3xl flex flex-col items-start justify-start p-8'>
+            <p className='text-3xl'>15,000+</p>
+            <p className='text-3xl'>Events booked</p>
+          </div>
+          <div className='w-full h-1/4  bg-black rounded-3xl text-white flex items-center justify-center gap-2 '>
+            <Sparkle className='size-8' />
+            <p className='font-semibold text-2xl'>Artist Unite</p>
+          </div>
         </div>
-        <div className='w-full h-68 bg-red-50 rounded-3xl'></div>
-        <div className='w-full h-44 bg-red-50 rounded-3xl'></div>
-        <div className='w-full h-60 bg-red-50 rounded-3xl'></div>
+        <div
+          className='w-full h-[17rem] bg-red-50 rounded-3xl relative'
+          style={{
+            backgroundImage: `url('https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            filter: "brightness(80%)"
+          }}
+        >
+          <p className="absolute text-white font-bold text-4xl bottom-8 left-8">Concerts</p>
+        </div>
+        <div className='w-full h-44 bg-gray-300 rounded-3xl flex flex-col justify-center items-center'>
+          <p className='font-semibold text-3xl'>2,500+</p>
+          <p className='font-semibold text-3xl'>Active Artists</p>
+        </div>
+        <div
+          className='w-full h-[17rem] bg-red-50 rounded-3xl relative'
+          style={{
+            backgroundImage: `url('https://images.pexels.com/photos/1625355/pexels-photo-1625355.jpeg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            filter: "brightness(80%)"
+          }}
+        >
+          <p className="absolute text-white font-bold text-3xl top-6 left-8">Singers</p>
+        </div>
         <div className='w-full h-96 flex flex-col gap-2'>
-          <div className='w-full h-3/4 bg-red-50 rounded-3xl'></div>
-          <div className='w-full h-1/4  bg-red-50 rounded-3xl'></div>
+          <div className='w-full h-3/4 bg-primary rounded-3xl flex p-8'>
+            <p className="text-3xl">Available for over 25+ Countries</p>
+          </div>
+          <div className='w-full h-1/4  bg-green-900 rounded-3xl text-white flex items-center justify-center gap-2 '>
+            <NotebookPen className='size-8' />
+            <p className='font-semibold text-2xl'>Plan Events</p>
+          </div>
         </div>
       </div>
     </div>

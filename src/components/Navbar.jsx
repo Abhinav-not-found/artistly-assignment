51792ea84591f47bc0abc26eb8aac607a193a8ff
abhinav <abhinavisgreat18@gmail.com>
2025-06-24 16:23:12 +0530

    chore: updated ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,18 @@ import { ModeToggle } from "./mode-toggle";
 const Navbar = () => {
   return (
     <header className='p-2 px-4 flex justify-between items-center'>
-      <Link href={'/'} className='flex gap-1 items-center'>
+      <Link href={"/"} className='flex gap-1 items-center'>
         <p className='bg-secondary w-fit py-1 px-2.5 rounded-full font-extrabold'>
           A
         </p>
         <p className='text-secondary font-extrabold text-xl'>Artistly</p>
       </Link>
-      <ModeToggle/>
+      <div className="flex items-center gap-4">
+        <div>
+          <Link href={'/explore'}>Explore</Link>
+        </div>
+        <ModeToggle />
+      </div>
     </header>
   );
 };
